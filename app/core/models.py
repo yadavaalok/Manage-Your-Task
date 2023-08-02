a2137df59d2b5394f616b2104cb40ef4a99a4538
Alok <yadavalok929@gmail.com>
2023-08-02 14:48:08 +0530

    Created Event Model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,22 @@
+"""
+Define all models here
+"""
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    EVENT_TYPES = [
+        ("conference", "conference"),
+        ("seminar", "seminar"),
+        ("company party or meeting", "company party or meeting"),
+        ("product or service launch", "product or service launch"),
+        ("wedding", "wedding"),
+        ("festival", "festival"),
+        ("exhibition", "exhibition"),
+        ("charity event", "charity event"),
+        ("sport and competition", "sport and competition"),
+    ]
+    event_title = models.CharField(max_length=255)
+    event_type = models.CharField(max_length=50, choices=EVENT_TYPES)
+    event_date = models.DateTimeField()
+    event_description = models.TextField()
