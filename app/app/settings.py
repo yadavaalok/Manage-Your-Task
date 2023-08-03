54df769158b655a64c5008af6a9bc5d46514370f
Alok <yadavalok929@gmail.com>
2023-08-03 18:55:57 +0530

    Configured templates and static files

--- app/app/settings.py
+++ app/app/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+   ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- app/task/views.py
+++ app/task/views.py
@@ -4,4 +4,4 @@ from django.views import View
 class Index(View):
 
 	def get(self,request):
-		return render(request,'base.html')
+		return render(request,'dashboard.html')
