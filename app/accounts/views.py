7765556d437787e3a04c73e35e4e67411bc3f77d
Alok <yadavalok929@gmail.com>
2023-08-09 12:26:02 +0530

    Implemented views for authentication

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.views import View
 
 
@@ -8,14 +9,52 @@ class Register(View):
     def get(self, request):
         return render(request, "register.html")
     
+    def post(self, request):
+        user_data = request.POST
+        first_name = user_data.get('first_name')
+        last_name = user_data.get('last_name')
+        username = user_data.get('username')
+        email = user_data.get('email')
+        password = user_data.get('password')
+        confirm_password = user_data.get('confirm_password')
+
+        if password!=confirm_password:
+            return render(request, "register.html")
+        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+        user.save()
+        return redirect('login')
+    
 
 class Login(View):
 
     def get(self, request):
         return render(request, "login.html")
     
+    def post(self, request):
+        user_data = request.POST
+        username = user_data.get('username')
+        password = user_data.get('password')
+        print()
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            return redirect('homepage')
+        else:
+            return render(request, "login.html")
+    
 
 class ResetPassword(View):
 
     def get(self, request):
         return render(request, "reset_password.html")
+    
+    def post(self, request):
+        user_data = request.POST
+        user = User.objects.get(username=user_data.get('username'))
+        if user is not None:
+            if user.email == user_data.get('email'):
+                if user_data.get('password') == user_data.get('confirm_password'):
+                    user.set_password(user_data.get('password'))
+                    user.save()
+                    return redirect('login')
+        return render(request, 'reset_password.html')
